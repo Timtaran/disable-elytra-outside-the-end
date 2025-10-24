a67321573c8f88e3f84ba8c124a1629aee848c35
timtaran <tentaranka@gmail.com>
2025-10-24 19:00:15 +0500

    feat: make this run

---
+++ src/main/java/io/github/timtaran/deote/platform/PlatformEntrypoint.java
@@ -0,0 +1,23 @@
+package io.github.timtaran.deote.platform;
+
+import io.github.timtaran.deote.DisableElytraOutsideTheEnd;
+/*? if fabric {*/
+import net.fabricmc.api.ModInitializer;
+
+public class PlatformEntrypoint implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        DisableElytraOutsideTheEnd.LOGGER.info("Fabric Entry Point");
+    }
+}
+/*?} elif neoforge {*/
+/*import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(DisableElytraOutsideTheEnd.MOD_ID)
+public class PlatformEntrypoint {
+    public PlatformEntrypoint(IEventBus modEventBus) {
+        DisableElytraOutsideTheEnd.LOGGER.info("NeoForge Entry Point");
+    }
+}
+*/
